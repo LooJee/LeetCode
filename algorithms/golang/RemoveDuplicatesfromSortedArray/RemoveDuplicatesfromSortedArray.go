60973a7f14d85da245f0f3e773c88075f1754b04
zer0 <mynameislujie1992@gmail.com>
2021-06-19 13:38:05 +0800

    [fix]

--- algorithms/golang/RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.go
+++ algorithms/golang/RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.go
@@ -3,6 +3,9 @@ package RemoveDuplicatesfromSortedArray
 func removeDuplicates(nums []int) int {
 	j := 0
 	numLen := len(nums)
+	if numLen == 0 {
+		return 0
+	}
 
 	for i := 0; i < numLen; i++ {
 		if nums[i] != nums[j] {
