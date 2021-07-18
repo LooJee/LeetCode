6fd7d63cd81803fccf26a493baec10456af5b1e4
zer0 <mynameislujie1992@gmail.com>
2021-07-18 11:38:32 +0800

    155 min stack

---
+++ algorithms/min_stack/min_stack.go
@@ -0,0 +1,40 @@
+package min_stack
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{
+		stack:    make([]int, 0),
+		minStack: make([]int, 0),
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 {
+		this.minStack = append(this.minStack, val)
+	} else {
+		min := this.GetMin()
+		if min > val {
+			min = val
+		}
+		this.minStack = append(this.minStack, min)
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
