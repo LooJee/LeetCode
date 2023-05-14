1ffa4f9d4d8d4cece23eba6119b4978e9c6e8185
zer0 <mynameislujie1992@gmail.com>
2023-05-14 22:58:50 +0800

    fix: remove nth from end of list

--- algorithms/golang/remove_nth_from_end/remove_nth_from_end.go
+++ algorithms/golang/remove_nth_from_end/remove_nth_from_end.go
@@ -5,32 +5,27 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 需要删除倒数第 n 个节点，实际是要找到倒数第 n+1 个节点，然后将其的 Next 指向倒数第 n-1 个节点。
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	if head == nil {
-		return nil
-	}
-	lastNode := head
-	l := 1
-	for lastNode.Next != nil {
-		lastNode = lastNode.Next
-		l++
+	var dummyHead = &ListNode{Next: head}
+	var x, y = head, dummyHead
+
+	// 新建一个指针 x 从 head 开始找到第 n+1 个元素，此时剩余的元素个数是 len - n
+	for i := 1; i < n && x != nil; i++ {
+		x = x.Next
 	}
 
-	if l == n {
-		head = head.Next
+	if x == nil {
 		return head
 	}
 
-	i := 1
-	cursor := head
-	for i < l-n {
-		cursor = cursor.Next
-		i++
+	//一直找到最后一个元素
+	for x.Next != nil {
+		x = x.Next
+		y = y.Next
 	}
 
-	if cursor.Next != nil {
-		cursor.Next = cursor.Next.Next
-	}
+	y.Next = y.Next.Next
 
-	return head
+	return dummyHead.Next
 }
