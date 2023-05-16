6087ac73f63d25185863ec0050a5957d7b1aa88e
zer0 <mynameislujie1992@gmail.com>
2023-05-16 23:39:04 +0800

    fix: 167. Two Sum II - Input Array Is Sorted

--- algorithms/golang/TwoSumII/TwoSumII.go
+++ algorithms/golang/TwoSumII/TwoSumII.go
@@ -1,24 +1,24 @@
 package TwoSumII
 
 /*
-	因为传入的数组已经排序，所以，除了使用在TwoSum中采用map来记录已经遍历过的值来实现O(n)的算法，
-	本例还可以使用i,j两个索引分别指向首尾，当两个数的和小于target时，向后移动i索引；当和大于target时，向前移动j索引，直到和与target相等
+因为传入的数组已经排序，所以，除了使用在TwoSum中采用map来记录已经遍历过的值来实现O(n)的算法，
+本例还可以使用i,j两个索引分别指向首尾，当两个数的和小于target时，向后移动i索引；当和大于target时，向前移动j索引，直到和与target相等
 */
 func twoSum(numbers []int, target int) []int {
-	var ret []int
-	i, j := 0, len(numbers)-1
+	var (
+		left, right = 0, len(numbers) - 1
+	)
 
-	for i != j {
-		sum := numbers[i] + numbers[j]
+	for left < right {
+		sum := numbers[left] + numbers[right]
 		if sum == target {
-			ret = []int{i + 1, j + 1}
-			break
+			return []int{left + 1, right + 1}
 		} else if sum < target {
-			i++
+			left++
 		} else {
-			j--
+			right --
 		}
 	}
 
-	return ret
+	return []int{}
 }
