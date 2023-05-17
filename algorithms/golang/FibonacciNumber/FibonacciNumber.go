5af6542d685d20058cca8073ebe62627045d6910
zer0 <mynameislujie1992@gmail.com>
2023-05-17 23:54:46 +0800

    fix: 509. Fibonacci Number optimize dp table

--- algorithms/golang/FibonacciNumber/FibonacciNumber.go
+++ algorithms/golang/FibonacciNumber/FibonacciNumber.go
@@ -1,18 +1,15 @@
 package FibonacciNumber
 
 func fib(n int) int {
-	if n == 0 {
-		return 0
+	if n == 0 || n == 1 {
+		return n
 	}
 
-	// 容量比 n 大 1， 避免 n 为 1 时初始化 dp[1] 发生数组越界
-	dp := make([]int, n+1)
-
-	dp[0], dp[1] = 0, 1
+	a, b := 0, 1
 
 	for i := 2; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+		a, b = b, a+b
 	}
 
-	return dp[n]
+	return b
 }
