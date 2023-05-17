850dd410fd1c877af09dd5859458c145b6ff16ac
zer0 <mynameislujie1992@gmail.com>
2023-05-17 23:49:53 +0800

    fix: 509. Fibonacci Number by dp

--- algorithms/golang/FibonacciNumber/FibonacciNumber.go
+++ algorithms/golang/FibonacciNumber/FibonacciNumber.go
@@ -1,15 +1,18 @@
 package FibonacciNumber
 
-func fib(N int) int {
-	if N == 0 || N == 1 {
-		return N
+func fib(n int) int {
+	if n == 0 {
+		return 0
 	}
 
-	a := 0
-	b := 1
-	for i := 1; i < N; i++ {
-		a, b = b, a+b
+	// 容量比 n 大 1， 避免 n 为 1 时初始化 dp[1] 发生数组越界
+	dp := make([]int, n+1)
+
+	dp[0], dp[1] = 0, 1
+
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
 
-	return b
+	return dp[n]
 }
