f8729d0e2fcbe28efdcbcd32a867af99f045e4cd
zer0 <mynameislujie1992@gmail.com>
2023-05-18 18:43:00 +0800

    fix: 322. Coin Change dp version

--- algorithms/golang/coin_change/main.go
+++ algorithms/golang/coin_change/main.go
@@ -3,44 +3,28 @@ package coinchange
 import "math"
 
 func coinChange(coins []int, amount int) int {
-	memos := make([]int, amount+1)
-	for idx := range memos {
-		memos[idx] = -2
-	}
-	return dp(coins, memos, amount)
-}
-
-func dp(coins, memos []int, amount int) int {
-	if amount == 0 {
-		return 0
+	dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = amount + 1
 	}
 
-	if amount < 0 {
-		return -1
-	}
+	// base case
+	dp[0] = 0
 
-	if memos[amount] != -2 {
-		return memos[amount]
-	}
+	// 自底向上求值
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i-coin < 0 {
+				continue
+			}
 
-	minCoins := math.MaxInt
-
-	for _, coin := range coins {
-		sub := amount - coin
-		ret := dp(coins, memos, sub)
-		if ret == -1 {
-			continue
+			dp[i] = int(math.Min(float64(dp[i]), float64(dp[i-coin]+1)))
 		}
-
-		minCoins = int(math.Min(float64(minCoins), float64(ret+1)))
 	}
 
-	if minCoins == math.MaxInt {
-		memos[amount] = -1
-		return -1
-	} else {
-		memos[amount] = minCoins
+	if dp[amount] == amount+1 {
+		dp[amount] = -1
 	}
 
-	return memos[amount]
+	return dp[amount]
 }
