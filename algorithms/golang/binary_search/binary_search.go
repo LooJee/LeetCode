61ff11f6760e3eaecf2e507b9e5895894949e39c
zer0 <mynameislujie1992@gmail.com>
2023-05-21 22:32:59 +0800

    fix: 704. Binary Search

--- algorithms/golang/binary_search/binary_search.go
+++ algorithms/golang/binary_search/binary_search.go
@@ -1,20 +1,22 @@
-package binary_search
+package binarysearch
 
 func search(nums []int, target int) int {
-	pos := -1
+	var (
+		left, right = 0, len(nums) - 1
+	)
 
-	l := 0
-	r := len(nums) - 1
-	for l <= r {
-		mid := (l + r) / 2
-		if nums[mid] < target {
-			l = mid + 1
-		} else if nums[mid] > target {
-			r = mid - 1
-		} else {
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
 			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
 		}
+
 	}
 
-	return pos
+	return -1
 }
--- algorithms/golang/binary_search/binary_search_test.go
+++ algorithms/golang/binary_search/binary_search_test.go
@@ -1,4 +1,4 @@
-package binary_search
+package binarysearch
 
 import "testing"
 
