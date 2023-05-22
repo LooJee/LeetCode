de23bc16ca80c048d874b432d257de520485a026
zer0 <mynameislujie1992@gmail.com>
2023-05-22 22:56:41 +0800

    fix: 34. Find First and Last Position of Element in Sorted Array by binary search

--- algorithms/golang/search_range/search_range.go
+++ algorithms/golang/search_range/search_range.go
@@ -1,25 +1,66 @@
 package searchrange
 
-import "math"
-
 func searchRange(nums []int, target int) []int {
-	var (
-		l, r = math.MaxInt, -1
-	)
-
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == target && i < l {
-			l = i
-		}
-
-		if nums[i] == target && i > r {
-			r = i
-		}
-	}
-
-	if l == math.MaxInt {
-		l, r = -1, -1
-	}
+	l := leftBound(nums, target)
+	r := rightBound(nums, target)
 
 	return []int{l, r}
+}
+
+func leftBound(nums []int, target int) int {
+	var (
+		l, r = 0, len(nums) - 1
+	)
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] == target {
+			// r 最终位置会到 target 左侧边界的前一个位置
+			r = mid - 1
+		}
+	}
+
+	// 说明没有找到过值为 target 的元素
+	if l == len(nums) {
+		return -1
+	}
+
+	if nums[l] != target {
+		return -1
+	}
+
+	return l
+}
+
+func rightBound(nums []int, target int) int {
+	var (
+		l, r = 0, len(nums) - 1
+	)
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] == target {
+			l = mid + 1
+		}
+	}
+
+	if l == 0 {
+		return -1
+	}
+
+	if nums[l-1] != target {
+		return -1
+	}
+
+	return l - 1
 }
