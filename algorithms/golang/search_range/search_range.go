aded8e29a52bac5e6a01d840f83bd1618cf15368
zer0 <mynameislujie1992@gmail.com>
2023-05-22 21:02:08 +0800

    fix: 34. Find First and Last Position of Element in Sorted Array

--- algorithms/golang/search_range/search_range.go
+++ algorithms/golang/search_range/search_range.go
@@ -1,73 +1,24 @@
-package search_range
+package searchrange
 
 import "math"
 
 func searchRange(nums []int, target int) []int {
-	l := searchLeft(nums, target)
-	r := searchRight(nums, target)
-
-	return []int{l, r}
-}
-
-func searchLeft(nums []int, target int) int {
-	l := 0
-	r := len(nums) - 1
-	pos := -1
-
-	var mid int
-	for l <= r {
-		mid = (l + r) / 2
-		if nums[mid] < target {
-			l = mid + 1
-		} else {
-			if nums[mid] == target {
-				pos = mid
-			}
-			r = mid - 1
-		}
-	}
-
-	return pos
-}
-
-func searchRight(nums []int, target int) int {
-	l := 0
-	r := len(nums) - 1
-	pos := -1
-
-	var mid int
-	for l <= r {
-		mid = (l + r) / 2
-		if nums[mid] > target {
-			r = mid - 1
-		} else {
-			if nums[mid] == target {
-				pos = mid
-			}
-			l = mid + 1
-		}
-	}
-
-	return pos
-}
-
-func searchRangeOnce(nums []int, target int) []int {
-	l, r := math.MaxInt, -1
+	var (
+		l, r = math.MaxInt, -1
+	)
 
 	for i := 0; i < len(nums); i++ {
-		if nums[i] == target {
-			if i < l {
-				l = i
-			}
+		if nums[i] == target && i < l {
+			l = i
+		}
 
-			if i > r {
-				r = i
-			}
+		if nums[i] == target && i > r {
+			r = i
 		}
 	}
 
 	if l == math.MaxInt {
-		l = -1
+		l, r = -1, -1
 	}
 
 	return []int{l, r}
--- algorithms/golang/search_range/search_range_test.go
+++ algorithms/golang/search_range/search_range_test.go
@@ -1,8 +1,7 @@
-package search_range
+package searchrange
 
 import "testing"
 
 func TestSearchRange(t *testing.T) {
 	t.Log(searchRange([]int{5, 7, 7, 8, 8, 10}, 8))
-	t.Log(searchRangeOnce([]int{5, 7, 7, 8, 8, 10}, 8))
 }
