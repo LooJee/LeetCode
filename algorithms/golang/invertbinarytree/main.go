1d07f1fb7cb9a0b4319121389661a4e22b62d26c
zer0 <mynameislujie1992@gmail.com>
2023-06-03 14:40:04 +0800

    feat: 226. Invert Binary Tree

---
+++ algorithms/golang/invertbinarytree/main.go
@@ -0,0 +1,29 @@
+package invertbinarytree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	root.Left, root.Right = root.Right, root.Left
+
+	return root
+}
