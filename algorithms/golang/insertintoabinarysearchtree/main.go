c9795c23eeb28dbd5922b4160134792a4aa1b838
zer0 <mynameislujie1992@gmail.com>
2023-06-05 01:09:23 +0800

    feat: 701. Insert into a Binary Search Tree

---
+++ algorithms/golang/insertintoabinarysearchtree/main.go
@@ -0,0 +1,29 @@
+package insertintoabinarysearchtree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	if val > root.Val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
+}
