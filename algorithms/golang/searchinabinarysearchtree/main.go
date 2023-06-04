46ada126f334ba48d996a2996c162522525a7a13
zer0 <mynameislujie1992@gmail.com>
2023-06-05 01:00:30 +0800

    feat: 700. Search in a Binary Search Tree

---
+++ algorithms/golang/searchinabinarysearchtree/main.go
@@ -0,0 +1,32 @@
+package searchinabinarysearchtree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}
